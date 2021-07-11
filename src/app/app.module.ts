9c3320bf7150a58f836e1e016fd4420930584d39
SAURAV KC <saurav.kc14@myhunter.cuny.edu>
2021-07-11 13:43:01 -0400

    Added components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { StudentAddComponent } from './components/student-add/student-add.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentEditComponent } from './components/student-edit/student-edit.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentAddComponent,
+    StudentListComponent,
+    StudentEditComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
