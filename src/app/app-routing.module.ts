9473a1487f223e1999ccd5f4ba0cf78f4da204c9
SAURAV KC <saurav.kc14@myhunter.cuny.edu>
2021-07-12 20:02:14 -0400

    Added routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { StudentAddComponent } from './components/student-add/student-add.component';
+import { StudentEditComponent } from './components/student-edit/student-edit.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'add-student', component: StudentAddComponent},
+  {path: 'list-student', component: StudentListComponent},
+  {path: 'edit-student', component: StudentEditComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
